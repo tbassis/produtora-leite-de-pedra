6d55e2ffda2489520f070aae3b0003117230f873
tbassis <tulioassis.18@gmail.com>
2025-01-10 11:39:20 -0300

    Add link to services

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,6 +8,9 @@ export default function Footer() {
           <Link href="/" className="text-xl font-bold text-white">Leite de Pedra</Link>
           <p className="text-sm text-gray-300 mt-2">Produção Cultural</p>
         </div>
+        <nav className="flex flex-wrap justify-center md:justify-end space-x-6">
+          <Link href="#services" className="text-gray-300 hover:text-gray-700">Serviços</Link>
+        </nav>
       </div>
       <div className="mt-8 text-center text-sm text-gray-200">
         © {new Date().getFullYear()} Leite de Pedra. Todos os direitos reservados ©.
